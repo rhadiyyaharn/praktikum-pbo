e1687c8d62748b045bc3ed8c9d56e627fb716596
rhadiyyaharn <100409805+rhadiyyaharn@users.noreply.github.com>
2023-05-04 22:49:19 +0700

    Update Car.java

--- praktikum 7/Inclusion_1/Car.java
+++ praktikum 7/Inclusion_1/Car.java
@@ -1,3 +1,9 @@
+/*
+* Nama File         : Car.java
+* Tanggal pembuatan : 03/04/2023
+* Penulis           : Rhadiyyah Ariana Saraswati
+*/
+
 public class Car extends Vehicle {
     void calRent(int jarak, float harga){
         float fare=jarak*harga;
