e209c0c7985802a2b5589b13e86f1068ae478f2c
rhadiyyaharn <100409805+rhadiyyaharn@users.noreply.github.com>
2023-05-04 22:50:24 +0700

    Update Vehicle.java

--- praktikum 7/Inclusion_1/Vehicle.java
+++ praktikum 7/Inclusion_1/Vehicle.java
@@ -1,3 +1,9 @@
+/*
+* Nama File         : Vehicle.java
+* Tanggal pembuatan : 03/04/2023
+* Penulis           : Rhadiyyah Ariana Saraswati
+*/
+
 public class Vehicle {
     void calRent(int distance, float price){
         float fare = distance * price;
