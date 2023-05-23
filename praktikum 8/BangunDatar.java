115a3e225e0451ed618a768f1794a40ef5cca5b1
rhadiyyaharn <100409805+rhadiyyaharn@users.noreply.github.com>
2023-05-24 00:18:27 +0700

    Update BangunDatar.java

--- praktikum 8/BangunDatar.java
+++ praktikum 8/BangunDatar.java
@@ -1,8 +1,8 @@
 /**
-* Nama File			: BangunDatar.java 
-* Tanggal Pembuatan : 17/05/2023
-* Penulis			: Rhadiyyah Ariana Saraswati
-* Deskripsi			: Kelas abstrak untuk bangun datar
+* Nama File		: BangunDatar.java 
+* Tanggal Pembuatan 	: 17/05/2023
+* Penulis		: Rhadiyyah Ariana Saraswati
+* Deskripsi		: Kelas abstrak untuk bangun datar
 */
 
 public abstract class BangunDatar{
