e087066835ce9b15d4e876f57b853866a1e48088
rhadiyyaharn <100409805+rhadiyyaharn@users.noreply.github.com>
2023-05-24 00:17:52 +0700

    Update ArrayListTest.java

--- praktikum 8/ArrayListTest.java
+++ praktikum 8/ArrayListTest.java
@@ -1,8 +1,8 @@
 /**
-* Nama File			: ArrayListTest.java 
-* Tanggal Pembuatan : 17/05/2023
-* Penulis			: Rhadiyyah Ariana Saraswati
-* Deskripsi			: Program penggunaan objek ArrayList sebagai Collection class
+* Nama File		: ArrayListTest.java 
+* Tanggal Pembuatan 	: 17/05/2023
+* Penulis		: Rhadiyyah Ariana Saraswati
+* Deskripsi		: Program penggunaan objek ArrayList sebagai Collection class
 */
 
 import java.util.ArrayList;
